76d1678ec6a8b060cfca3b4caae4217a5112fde7
Unknown <nosrednaski@gmail.com>
2018-08-25 21:43:35 -0400

    Layout
    * Collapsible Result Tables
    * Result Chart
    * White background
    * Results Card Layout - NEED TO BE HOOKED UP
    * User Searches Card Layout - NEED TO BE HOOKED UP
    * Materialize Icons

--- public/js/index.js
+++ public/js/index.js
@@ -1,106 +1,5 @@
-// // Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// // refreshExamples gets new examples from the db and repopulates the list
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// // handleFormSubmit is called whenever we submit a new example
-// // Save the new example to the db and refresh the list
-// var handleFormSubmit = function(event) {
-//   event.preventDefault();
-
-//   var example = {
-//     text: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
-//   }
-
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
-
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
-
-// // handleDeleteBtnClick is called when an example's delete button is clicked
-// // Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// // Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
-
-// //--------------------------------------//
-
 //Functions to hide and show containers//
+
 
 const hideTableContainer = function() {
   $(".table-container").hide();
@@ -114,9 +13,11 @@ const hideTableContainer = function() {
 
 //Front-end JavaScript//
 $(document).ready(function(){
-  hideTableContainer();
+  // hideTableContainer();
+  //   //######################
+  $("#resultsContainer").show();
 
-  $("#resultsContainer").hide();
+  // $("#resultsContainer").hide();
   
 
   $('#myModal').on('shown.bs.modal', function () {
@@ -141,6 +42,12 @@ $(document).ready(function(){
 });
 
 //inits materialize features
+// Makes Collapsible werk werk werk werk werk
+const elem = document.querySelector('.collapsible.expandable');
+const instance = M.Collapsible.init(elem, {
+  accordion: false
+});
+
 $(document).ready(function(){
   $('.tabs').tabs();  
 });
@@ -156,6 +63,12 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('select').formSelect();
 });
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+
 
 
 const select = $("select");  //Variable that helps clear selects
@@ -183,6 +96,25 @@ $(document).ready(function(){
         url: "/api/"+country+"/"+category+"?goal1="+min_goal+"&goal2="+max_goal,
         method: "get"}).then(function(response) {
         console.log(response);
+        
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+          var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Success', response.successPercentage],  
+            ['Failure',  response.failurePercentage],
+          ]);
+
+          var options = {
+            title: 'Success/ Failure'
+          };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
       });
 
       // CLEAR FORM
@@ -202,23 +134,5 @@ select.formSelect();
 //Draw pie chart with success/ failure in results
 
     
-      // google.charts.load('current', {'packages':['corechart']});
-      // google.charts.setOnLoadCallback(drawChart);
-
-      // function drawChart() {
-
-      //   var data = google.visualization.arrayToDataTable([
-      //     ['Task', 'Hours per Day'],
-      //     ['Success', response.successPercentage],  
-      //     ['Failure',  response.failurePercentage],
-      //   ]);
-
-      //   var options = {
-      //     title: 'Success/ Failure'
-      //   };
-
-      //   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-      //   chart.draw(data, options);
-      // }
+      
    
